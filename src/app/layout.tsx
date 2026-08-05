44261ba1b7b28c1cea26688b6c9ac9a073e762b3
Vijay Pratap Singh <vijaypratap218@gmail.com>
2026-08-05 17:30:57 +0530

    Updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -106,6 +106,19 @@ export default function RootLayout({
             })(window, document, "clarity", "script", "xxlaecvsng");
           `}
         </Script>
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6a731f8063910b1d443c296b/1jv8r63rg';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </head>
       <body className="min-h-full flex flex-col bg-background text-foreground">
         {jsonLd.map((schema, i) => (
